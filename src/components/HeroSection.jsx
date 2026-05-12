452c7a90697728a89d718c4073872f95aab30523
anjudixit <163434535+anjudixit@users.noreply.github.com>
2026-05-12 11:16:10 +0530

    improve UI responsiveness and card styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,20 +8,21 @@ import TrendyPlantsHeading from "./TrendyPlantsHeading";
 
 const HeroSection = () => {
   return (
-    <section className="relative w-full min-h-[190vh] md:min-h-[180vh] lg:min-h-[200vh] bg-[#182015]">
+    <section className="relative w-full min-h-[250vh] sm:min-h-[220vh] md:min-h-[180vh] lg:min-h-[200vh] bg-[#182015]">
+
       <Banner_Image />
       <MainHeading />
       <PlantCard />
       <ReviewCard />
-      <div className="absolute top-[74vh] md:top-[80vh] lg:top-[100vh] left-1/2 -translate-x-1/2 z-10">
-        <TrendyPlantsHeading title="Our Trendy Plants" />
+
+      <div className=" absolute top-[92vh] sm:top-[85vh] md:top-[80vh] lg:top-[100vh] left-1/2 -translate-x-1/2 z-10">
+      <TrendyPlantsHeading title="Our Trendy Plants" />
       </div>
 
-      <div className="absolute top-[90vh] md:top-[95vh] lg:top-[112vh] left-0 w-full flex flex-col gap-8 md:gap-14 px-4 md:px-8 z-10">
+      <div className=" absolute top-[108vh] sm:top-[100vh] md:top-[95vh] lg:top-[112vh] left-0 w-full flex flex-col gap-14 md:gap-14 px-4 md:px-8 z-10">
         <TrendyPlantCard1 />
         <TrendyPlantCard2 />
       </div>
-
     </section>
   );
 };
